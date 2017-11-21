b326943e121e3847b6df0139e0eef3c08adc132f
Kieran Kuhn <kuhn.kieran@gmail.com>
2017-11-21 00:13:36 -0700

    Converted to Materialize

---
+++ assets/app/app.js
@@ -0,0 +1,14 @@
+// Initialize collapse button
+$(".button-collapse").sideNav();
+// Show sideNav
+$('.button-collapse').sideNav('show');
+// Hide sideNav
+$('.button-collapse').sideNav('hide');
+// Destroy sideNav
+$('.button-collapse').sideNav('destroy');
+
+$('.carousel.carousel-slider').carousel({fullWidth: true});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
